66975650206079dd0e46e4f1acaa7933a2db75b6
Kulhanek László <kulh.laszlo@gmail.com>
2023-07-08 23:45:32 +0200

    Small changes

--- src/Controller/ContactController.php
+++ src/Controller/ContactController.php
@@ -3,17 +3,35 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Form\FormError;
 use App\Entity\Messages;
 use App\Form\ContactFormType;
 
 class ContactController extends AbstractController
 {
     #[Route('/', name: 'contact')]
-    public function index(): Response
+    public function index(Request $request): Response
     {
         $message = new Messages();
-        $form = $this->createForm(ContactFormType::class, $message);
+
+        $form = $this->createForm(ContactFormType::class, $message,[
+            'method' => 'POST',
+        ]);
+        $form->handleRequest($request);
+
+        $successMessage = null;
+        if ($form->isSubmitted())
+        {
+            if ($form->isValid())
+            {
+                $contactForm = $form->getData();
+            }else{
+                $form->addError(new FormError('Hiba! Kérjük töltsd ki az összes mezőt!'));
+            }
+        }
+
         return $this->render('contact\index.html.twig',[
             'form' => $form,
         ]);
--- src/Form/ContactFormType.php
+++ src/Form/ContactFormType.php
@@ -2,44 +2,56 @@
 
 namespace App\Form;
 
+use App\Entity\Messages;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
-use App\Entity\Messages;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\NotBlank;
 
 class ContactFormType extends AbstractType
 {
-    public function buildForm(FormBuilderInterface $builder, array $options)
+    public function buildForm(FormBuilderInterface $builder, array $options) : void
     {
         $builder
             ->add('name', TextType::class, [
                 'label' => 'Neved: ',
                 'row_attr' => [
                     'class' => 'mb-3',
-                ]
+                ],
+                'required' => false,
+                'constraints' => [
+                    new NotBlank()
+                ],
             ])
             ->add('email', EmailType::class, [
                 'label' => 'E-mail címed: ',
                 'row_attr' => [
                     'class' => 'mb-3',
-                ]
+                ],
+                'required' => false,
+                'constraints' => [
+                    new NotBlank()
+                ],
             ])
             ->add('message', TextareaType::class, [
                 'label' => 'Üzenet szövege: ',
                 'row_attr' => [
                     'class' => 'mb-3',
-                ]
+                ],
+                'required' => false,
+                'constraints' => [
+                    new NotBlank()
+                ],
             ])
             ->add('submit', SubmitType::class, [
+                'label' => 'Küldés',
                 'row_attr' => [
-                    'class' => 'w-50 d-inline-block,',
+                    'class' => 'w-50 d-inline-block',
                 ],
-                'attr' => ['class' => 'btn-success'],
-                'label' => 'Üzenet küldése',
             ]);
     }
 
